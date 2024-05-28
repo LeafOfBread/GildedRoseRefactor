4149fe354f8c34757b19d4a86d6fbf0f4cf68a1d
Daniel Gebhart <daniel.gebhart@hotmail.com>
2024-05-28 21:48:55 +0200

    Two test cases added

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -6,5 +6,17 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("fixme");
   });
+  it("should decrease the quality and sellIn of a normal item", function() {
+    const gildedRose = new Shop([ new Item("foo", 10, 20) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual("foo");
+    expect(items[0].sellIn).toEqual(9);
+    expect(items[0].quality).toEqual(19);
+  });
+  it("should not decrease the quality of an item below 0", function() {
+    const gildedRose = new Shop([ new Item("foo", 10, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  });
 
 });
