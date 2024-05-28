4a3cc5ade5eb2913055c5cddba7d668b766a6850
Daniel Gebhart <daniel.gebhart@hotmail.com>
2024-05-28 21:51:05 +0200

    last two unit tests added

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -47,5 +47,16 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(23);
   });
+  it("should decrease the quality of a normal item twice as fast after the sell by date", function() {
+    const gildedRose = new Shop([ new Item("foo", 0, 20) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(18);
+  });
+
+  it("should increase the quality of Aged Brie by 2 after the sell by date", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 0, 20) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(22);
+  });
 
 });
